00eb4031dae7e44c43b3bf962602c8600f2686e7
Marc <marcllopis89@gmail.com>
2020-12-16 10:10:58 +0100

    hooks version

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 
 const Movie = props => {
-  // Main on this component is to control if it's being rendered on '/' or in '/random'. If it is on '/' it will receive standard props, but if it is on '/random' we need to get those props from the react-router, so we control this with the following logic:
+
   let myProps = props.location
     ? props.location.state.randomMovie
     : props
@@ -15,7 +15,6 @@ const Movie = props => {
       <p>Genres:
         {myProps.genres.map((genre) => <span key={nanoid()}>{genre}</span>)}
       </p>
-      {/* If we receive the prop add or remove we show different buttons */}
       {myProps.add && <button id={myProps.index} onClick={myProps.action}>Add to favorites</button>}
       {myProps.remove && <button id={myProps.index} onClick={() => myProps.action(myProps.title)}>Remove from favorites</button>}
       <br />
